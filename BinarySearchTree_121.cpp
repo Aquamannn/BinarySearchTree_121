8de01606ef0340867583fa399aac9f5f7309eed4
Lenovo <9.3razzinayyuri@gmail.com>
2024-06-14 10:00:29 +0700

    void insert

--- BinarySearchTree_121.cpp
+++ BinarySearchTree_121.cpp
@@ -24,4 +24,21 @@ public:
 	{
 		ROOT = NULL; //Initializing ROOT to NULL
 	}
+
+	void insert(string element) //Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; //Assign velue to the data field of the new node
+		newNode->leftchild = NULL; //Make the left child of the node point to NULL
+		newNode->rightchild = NULL; //Make the right child of the node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL; 
+		search(element, perent, currentNode); //Locate the node which will be the parent of the node to be inserted
+		
+		
+
+	
+	
+	}
 };
